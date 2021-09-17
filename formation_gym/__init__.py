872d5625d6f3feb12d3415024daf1d726ab38b31
Reed Pan <reedpan@Reeds-MacBook-Pro.local>
2021-09-18 07:50:57 +0800

    add result

--- formation_gym/__init__.py
+++ formation_gym/__init__.py
@@ -1,6 +1,7 @@
 import imp
 from multiagent.environment import MultiAgentEnv
 import os.path as osp
+import numpy as np
 
 def make_env(scenario_name='basic_formation_env', benchmark=False):
     # load scenario from script
@@ -14,3 +15,25 @@ def make_env(scenario_name='basic_formation_env', benchmark=False):
     else:
         env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, shared_viewer = True)
     return env
+
+def ezpolicy(world):
+    eps = 0.1
+    act_n = []
+    u = np.zeros(5) # 5-d because of no-move action
+    for i in range(len(world.agents)):
+        move1 = True
+        move2 = True
+        if -(world.agents[i].state.p_pos[0] - world.landmarks[i].state.p_pos[0])>eps: 
+            u[1] += 0.5
+        elif (world.agents[i].state.p_pos[0] - world.landmarks[i].state.p_pos[0])>eps: 
+            u[2] += 0.5
+        else: move1 = False
+        if -(world.agents[i].state.p_pos[1] - world.landmarks[i].state.p_pos[1])>eps: 
+            u[3] += 0.5
+        elif (world.agents[i].state.p_pos[1] - world.landmarks[i].state.p_pos[1])>eps: 
+            u[4] += 0.5
+        else: move2 = False
+        u[0] += 1.0
+        act_n.append(np.concatenate([u, np.zeros(world.dim_c)]))
+    done = not (move1 and move2)
+    return done, act_n
--- interactive.py
+++ interactive.py
@@ -6,17 +6,27 @@ from multiagent.policy import InteractivePolicy
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('-s', '--scenario', default='basic_formation_env', help='Path of the scenario Python script.')
+    parser.add_argument('-d', '--demo', action='store_true', help='If show the demo.')
     args = parser.parse_args()
 
     env = formation_gym.make_env(args.scenario, True)
     env.render()
     policies = [InteractivePolicy(env,i) for i in range(env.n)]
     obs_n = env.reset()
+    cnt = 0
     while True:
         # query for action from each agent's policy
         act_n = []
-        for i, policy in enumerate(policies):
-            act_n.append(policy.action(obs_n[i]))
+        if args.demo:
+            done, act_n = formation_gym.ezpolicy(env.world)
+            # cnt+=1
+            if not done: cnt+=1
+            else:
+                print('total steps: ', cnt)
+                # break
+        else:
+            for i, policy in enumerate(policies):
+                act_n.append(policy.action(obs_n[i]))
         # step environment
         obs_n, reward_n, done_n, _ = env.step(act_n)
         # render all agent views
